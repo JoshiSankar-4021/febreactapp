bcf5057c8fbbe502f351189113622f723320c7b4
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-04-10 12:25:28 +0530

    animation2

--- src/animations/Animationsfile.jsx
+++ src/animations/Animationsfile.jsx
@@ -17,6 +17,41 @@ function Animationsfile(){
             <h1>SLIDE IN FROM LEFT</h1>
             <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
             </div>
+            <div className='slidein-right'>
+            <h1>SLIDE IN FROM RIGHT</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='slidein-top'>
+            <h1>SLIDE IN TOP</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='slidein-bottom'>
+            <h1>SLIDE IN Bottom</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='bounce'>
+            <h1>Bounce</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='pulse'>
+            <h1>Pulse</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='shake'>
+            <h1>Shake</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
+            <div className='rotate'>
+            <h1>rotate</h1>
+            <img src='https://imgd.aeplcdn.com/600x337/n/cw/ec/171777/kylaq-exterior-right-front-three-quarter-10.jpeg?isig=0&q=80'alt='' style={{height: '200px', width: '300px'}}/>
+            </div>
+
         </div>
     );
 }
